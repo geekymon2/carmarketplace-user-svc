08d819bce879161bb8be0f21bae197ee127510a5
Geeky Mon2 <geekymon2@gmail.com>
2022-06-23 19:46:16 +1000

    cleanup

--- src/main/java/com/geekymon2/carmarketplace/userservice/models/UserDto.java
+++ src/main/java/com/geekymon2/carmarketplace/userservice/models/UserDto.java
@@ -1,4 +1,19 @@
 package com.geekymon2.carmarketplace.userservice.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Boolean isActive;
+    private Boolean isVerified;
 }
