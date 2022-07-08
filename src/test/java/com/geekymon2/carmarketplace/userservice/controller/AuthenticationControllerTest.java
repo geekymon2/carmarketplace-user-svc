6292c9f7b8c6f8bd8ea0247c52b7ab29aa92421e
Geeky Mon2 <geekymon2@gmail.com>
2022-07-08 22:48:03 +1000

    updated

--- src/test/java/com/geekymon2/carmarketplace/userservice/controller/AuthenticationControllerTest.java
+++ src/test/java/com/geekymon2/carmarketplace/userservice/controller/AuthenticationControllerTest.java
@@ -5,13 +5,19 @@ import com.geekymon2.carmarketplace.userservice.models.JwtRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
 class AuthenticationControllerTest {
     private final AuthenticationController controller;
 
