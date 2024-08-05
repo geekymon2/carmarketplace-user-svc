fc23d2c5734c0ae8071b300da1a8c5b7bcef4285
geekymon2 <geekymon2@gmail.com>
2024-08-05 14:41:54 +1000

    Update AppUser.java

--- src/main/java/com/geekymon2/carmarketplace/userservice/entities/AppUser.java
+++ src/main/java/com/geekymon2/carmarketplace/userservice/entities/AppUser.java
@@ -1,6 +1,10 @@
 package com.geekymon2.carmarketplace.userservice.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
