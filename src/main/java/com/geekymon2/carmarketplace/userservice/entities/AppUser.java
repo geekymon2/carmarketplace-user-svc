d3f4184b3cbdfc1188b227397b46cdbc20e2ecaf
Geeky Mon2 <geekymon2@gmail.com>
2024-08-12 20:44:08 +1000

    Update AppUser.java

--- src/main/java/com/geekymon2/carmarketplace/userservice/entities/AppUser.java
+++ src/main/java/com/geekymon2/carmarketplace/userservice/entities/AppUser.java
@@ -23,17 +23,18 @@ public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "firstname is mandatory")
     private String firstname;
+    @NotBlank(message = "lastname is mandatory")
     private String lastname;
-
     @NotBlank(message = "email is mandatory")
     @Column(unique = true)
     private String email;
-
     @NotBlank(message = "password is mandatory")
     private String password;
     private Boolean isActive;
     private Boolean isVerified;
+    @NotBlank(message = "created date is mandatory")
     private Timestamp createdDate;
     private Timestamp lastLoginDate;
 }
