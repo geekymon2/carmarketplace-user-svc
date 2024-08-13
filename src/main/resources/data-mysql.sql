0f0c2f60ec7d5e710a4f6040048b84fe5efc6378
Geeky Mon2 <geekymon2@gmail.com>
2024-08-13 16:12:25 +1000

    fixed

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,3 +1,3 @@
 -- insert user
-INSERT INTO appuser(is_active, is_verified, created_date, email, firstname, lastname, password) VALUES ('Y','Y', '2024-01-01 10:00:00', 'geekymon2@gmail.com','Geeky', 'Mon2', 'password')
-INSERT INTO appuser(is_active, is_verified, created_date, email, firstname, lastname, password) VALUES ('Y','Y', '2024-01-01 10:00:00', 'testuser@gmail.com', 'Test', 'User', 'password')
+INSERT INTO appuser(is_active, is_verified, created_date, email, firstname, lastname, password) VALUES (true, true, '2024-01-01 10:00:00', 'geekymon2@gmail.com','Geeky', 'Mon2', 'password')
+INSERT INTO appuser(is_active, is_verified, created_date, email, firstname, lastname, password) VALUES (true, true, '2024-01-01 10:00:00', 'testuser@gmail.com', 'Test', 'User', 'password')
